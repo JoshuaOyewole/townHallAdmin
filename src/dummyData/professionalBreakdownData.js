c6d20837f5e2d6670c26e5fcf47cb3a517b8a0bf
joshua Oyewole <joshpp2013@gmail.com>
2023-10-21 01:24:34 +0100

    Added dummy Data

---
+++ src/dummyData/professionalBreakdownData.js
@@ -0,0 +1,52 @@
+export const professionalBreakdownData = {
+  title: "Professional Breakdown",
+  heading: ["Occupation", "Membership"],
+  data: [
+    {
+      occupation: "Civil Servant (Federal)",
+      membership: "22",
+    },
+    {
+      occupation: "Civil Servant (State)",
+      membership: "23",
+    },
+    {
+      occupation: "Student",
+      membership: "14",
+    },
+    {
+      occupation: "Coperate (I work for a company)",
+      membership: "9",
+    },
+    {
+      occupation: "Masters/PhD",
+      membership: "13",
+    },
+  ],
+};
+export const educationBreakdownData = {
+  title: "Education Breakdown",
+  heading: ["Level", "Membership"],
+  data: [
+    {
+      occupation: "Bachelor’s Degree",
+      membership: "22",
+    },
+    {
+      occupation: "Primary School",
+      membership: "23",
+    },
+    {
+      occupation: "Secondary School",
+      membership: "14",
+    },
+    {
+      occupation: "Higher National Diploma",
+      membership: "9",
+    },
+    {
+      occupation: "Trader/Artisan (Handwork)",
+      membership: "13",
+    },
+  ],
+};
