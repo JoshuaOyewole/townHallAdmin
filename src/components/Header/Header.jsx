f802e406531769cf9f5a0151dd65280695ffd15a
joshua Oyewole <joshpp2013@gmail.com>
2023-10-22 15:09:50 +0100

    modified NavMenu in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import { Link, NavLink } from "react-router-dom";
-import { logo, user_pixs, homeIcon, organizations, memberIcon, electionIcon } from "../../assets/assets";
 import Styles from "./_header.module.scss";
-import {IoMdNotificationsOutline} from "react-icons/io"
-import {BiMoon} from "react-icons/bi"
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { BiMoon } from "react-icons/bi";
+import { logo, user_pixs } from "../../assets/assets";
+import { navMenuData } from "../../dummyData/navMenu";
 
+export const NavItem = (props) => {
+  const { path, routeName, icon } = props;
 
+  return (
+    <li className={Styles.header__navmenu_list}>
+      <img src={icon} alt={`${routeName} icon`} />
+      <NavLink to={path} className="hover:text-[#2e9fc9]">
+        {routeName}
+      </NavLink>
+    </li>
+  );
+};
 
 function Header() {
   const admin_name = "Oluwadamilare";
@@ -14,20 +26,20 @@ function Header() {
       <div className="flex flex-col justify-between">
         <div className={Styles.header__top}>
           <div className={Styles.header__top_logo}>
-           <Link to="/">
-           <img
-              src={logo}
-              alt="TownHall logo"
-              className={Styles.header__logo}
-            />
-           </Link>
+            <Link to="/">
+              <img
+                src={logo}
+                alt="TownHall logo"
+                className={Styles.header__logo}
+              />
+            </Link>
           </div>
           <div className={Styles.header__top_userInfo}>
             <button className={Styles.icon}>
-              <BiMoon className='w-6 h-6 text-black' />
+              <BiMoon className="w-6 h-6 text-black" />
             </button>
             <button className={Styles.notification}>
-              <IoMdNotificationsOutline  className={Styles.icons}/>
+              <IoMdNotificationsOutline className={Styles.icons} />
             </button>
             <div className={Styles.header__top_user}>
               <div className={Styles.header__top_user_pixs}>
@@ -43,51 +55,16 @@ function Header() {
         <hr />
         <div className={Styles.header_bottom}>
           <ul className={Styles.header_bottom__navmenu}>
-            <li className={Styles.header__navmenu_list}>
-              <img src={homeIcon} alt="Home Icon" />
-              <NavLink
-                to="/"
-                className="hover:font-medium"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className={Styles.header__navmenu_list}>
-              <img src={organizations} alt="Home Icon" />
-              <NavLink
-                to="/organizations"
-                className="hover:font-medium"
-              >
-                Organizations
-              </NavLink>
-            </li>
-            <li className={Styles.header__navmenu_list}>
-            <img src={memberIcon} alt="Home Icon" />
-              <NavLink
-                to="/members"
-                className="hover:font-medium"
-              >
-                Members
-              </NavLink>
-            </li>
-            <li className={Styles.header__navmenu_list}>
-            <img src={memberIcon} alt="Home Icon" />
-              <NavLink
-                to="/post_management"
-                className="hover:font-medium"
-              >
-                Post Management
-              </NavLink>
-            </li>
-            <li className={Styles.header__navmenu_list}>
-            <img src={electionIcon} alt="Home Icon" />
-              <NavLink
-                to="/election"
-                className="hover:font-medium"
-              >
-                Election
-              </NavLink>
-            </li>
+            {navMenuData.map((nav, index) => {
+              return (
+                <NavItem
+                  path={nav.path}
+                  routeName={nav.routeName}
+                  icon={nav.icon}
+                  key={index}
+                />
+              );
+            })}
           </ul>
           <div className={Styles.header__search_container}>
             <input
